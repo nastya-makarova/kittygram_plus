674233487062a302299e85dedd1d85215d1a833c
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-09-13 12:08:33 +0400

    Added method action for searching 5 white cats.

--- cats/views.py
+++ cats/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
 
 from .models import Cat, Owner
@@ -7,6 +9,17 @@ from .serializers import CatSerializer, OwnerSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    # Пишем метод, а в декораторе разрешим работу со списком объектов
+    # и переопределим URL на более презентабельный
+    @action(detail=False, url_path='recent-white-cats')
+    def recent_white_cats(self, request):
+        # Нужно получить записи о пяти котиках белого цвета
+        cats = Cat.objects.filter(color='White')[:5]
+        # Передадим queryset cats сериализатору 
+        # и разрешим работу со списком объектов
+        serializer = self.get_serializer(cats, many=True)
+        return Response(serializer.data)
 
 
 class OwnerViewSet(viewsets.ModelViewSet):
